011d631b13712a1f6166ba49fb6594bc15fed573
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-10-15 11:45:54 -0500

    added spot migration and model file, added associations

---
+++ backend/db/models/spot.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model, Validator
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Spot.belongsTo(
+        models.User,
+        {foreignKey: 'ownerId'}
+      )
+    }
+  }
+  Spot.init({
+    ownerId: DataTypes.INTEGER,
+    address: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    country: DataTypes.STRING,
+    lat: DataTypes.DECIMAL,
+    lng: DataTypes.DECIMAL,
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
