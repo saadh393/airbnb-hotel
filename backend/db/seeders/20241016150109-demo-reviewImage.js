16e59dd15097cb9cdd31b410a1b244d7aee01273
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-10-16 10:10:07 -0500

    Add ReviewImage seeder file and successfully seeded database

---
+++ backend/db/seeders/20241016150109-demo-reviewImage.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// /** @type {import('sequelize-cli').Migration} */
+
+const { ReviewImage } = require("../models");
+
+let options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await ReviewImage.bulkCreate([
+      {
+        reviewId: 1,
+        url: "https://th.bing.com/th/id/R.e78f8e7c326d3e7cdcf053d58f494542?rik=bXopo7rm0XIdFQ&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fc%2fc7%2fDomestic_shorthaired_cat_face.jpg&ehk=NByReFekRNa%2fCe0v9gNPEb0tpYmVhy4kI5uaC1l1AUI%3d&risl=1&pid=ImgRaw&r=0"
+      },
+      {
+        reviewId: 2,
+        url: "https://static.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg"
+      },
+      {
+        reviewId: 3,
+        url: "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"
+      }
+    ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'ReviewImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      reviewId: { [Op.in]: [1, 2, 3] }
+    }, {});
+  }
+};
