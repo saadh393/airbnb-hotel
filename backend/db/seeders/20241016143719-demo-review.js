a9122cb9f2ad4dac7e58a253d0b55c61ebae2c79
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-10-16 09:46:10 -0500

    Add seeder file for Reviews table and successfully seed database

---
+++ backend/db/seeders/20241016143719-demo-review.js
@@ -0,0 +1,59 @@
+'use strict';
+
+// /** @type {import('sequelize-cli').Migration} */
+
+const { Review } = require('../models');
+
+let options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await Review.bulkCreate([
+      {
+        spotId: 1,
+        userId: 1,
+        review: "This was an awesome spot!",
+        stars: 5
+      },
+      {
+        spotId: 2,
+        userId: 2,
+        review: "This was an ok spot!",
+        stars: 2
+      },
+      {
+        spotId: 3,
+        userId: 3,
+        review: "This was a great spot!",
+        stars: 4
+      }
+    ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Reviews';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      userId: { [Op.in]: [1, 2, 3] }
+    }, {});
+  }
+};
