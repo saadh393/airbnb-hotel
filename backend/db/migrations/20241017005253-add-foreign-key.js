689116b6a345577433540a565d5bd03c81ba874a
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-10-16 19:53:57 -0500

    Add migration files to remove / re-add foreign key in order to circumvent build failure issue with foreign key on Render.com

---
+++ backend/db/migrations/20241017005253-add-foreign-key.js
@@ -0,0 +1,25 @@
+'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addConstraint('spotImages', {
+      fields: ['spotId'],
+      type: 'foreign key',
+      name: 'spotImages_spotId_fkey',
+      references: {
+        table: 'Spots',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    }, options);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('spotImages', 'spotImages_spotId_fkey', options);
+  }
+};
