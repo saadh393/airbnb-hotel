cef1485362722f544cdab51659b83d7f15c019d1
raihanhasan <your.email@susickjoarder@gmail.com>
2024-10-20 18:01:56 -0400

    making route for soptimages

--- backend/routes/api/spotImages.js
+++ backend/routes/api/spotImages.js
@@ -4,6 +4,23 @@ const { spotImage, Spot, User } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
 
+router.post('/:spotId/images',requireAuth,async(req,res,next)=>{
+    
+    try{
+        const userId = req.user.id;
+        const spotId = req.params.spotId;
+        const spot = await Spot.findByPk(spotId);
+        if(!spot){
+            return res.status(404).json({
+                message:"Spot couldn't be found"
+            })
+        }
+        
+    }
+    catch(err){
+        next(err)
+    }
 
+})
 
 module.exports = router;
