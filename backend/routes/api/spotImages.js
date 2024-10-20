fb8b765b5e7a4f9a5431335a7297694ff752d56b
raihanhasan <your.email@susickjoarder@gmail.com>
2024-10-20 19:11:20 -0400

    finished spotimages route

--- backend/routes/api/spotImages.js
+++ backend/routes/api/spotImages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { spotImage, Spot, User } = require('../../db/models');
+const { SpotImage, Spot, User } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
 
@@ -10,12 +10,27 @@ router.post('/:spotId/images',requireAuth,async(req,res,next)=>{
         const userId = req.user.id;
         const spotId = req.params.spotId;
         const spot = await Spot.findByPk(spotId);
+        console.log(spot);
         if(!spot){
             return res.status(404).json({
                 message:"Spot couldn't be found"
             })
+
+
         }
-        
+        if(spot.ownerId !== userId){
+            return res.status(403).json({
+                message:"Unauthorized"
+            })
+        }
+        const {url,preview} = req.body;
+        const newSpotImage = SpotImage.create({
+            spotId:spotId,
+            url,
+            preview
+
+        })
+        res.status(201).json(newSpotImage)
     }
     catch(err){
         next(err)
