bcaf46166057ca3174312ad681b562e89aced8a4
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-10-23 11:43:54 -0500

    added 'delete review image' route

--- backend/routes/api/reviewImages.js
+++ backend/routes/api/reviewImages.js
@@ -1,7 +1,31 @@
 const express = require('express');
 
-const { ReviewImage, Spot, User } = require('../../db/models');
+const { ReviewImage, Review, Spot, User } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
+
+router.delete("/:imageId", requireAuth, async (req, res, next) => {
+
+    try {
+
+        const userId = req.user.id;
+        const imageId = req.params.imageId;
+
+        const reviewImage = await ReviewImage.findByPk(imageId);
+
+        if (!reviewImage) return res.status(404).json({ message: "Review Image couldn't be found" });
+
+        const review = await Review.findByPk(reviewImage.reviewId);
+
+        if (review.userId !== userId) return res.status(403).json({ message: "Unauthorized" });
+
+        await reviewImage.destroy();
+
+        res.status(200).json({ message: "Successfully deleted" });
+
+    } catch (err) {
+        next(err)
+    }
+})
 
 module.exports = router;
