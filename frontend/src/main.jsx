bbe8fb6ed7310c75b92404483442cdbcc62c9867
raihanhasan <your.email@susickjoarder@gmail.com>
2024-11-22 12:22:25 -0500

    test push

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import './index.css';
-import configureStore from './store';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Provider } from "react-redux"
+import App from "./App"
+import "./index.css"
+import configureStore from "./store"
 
-const store = configureStore();
+const store = configureStore()
 
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
+if (process.env.NODE_ENV !== "production") {
+  window.store = store
 }
+//testing
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
-);
+)
